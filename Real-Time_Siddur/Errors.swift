6801ba873a20330e40b9eb66e84755fbbbcdbd34
Ezra Muskat <razeprime1@gmail.com>
2019-08-21 11:36:55 -0400

    Created an enum to hold any error messages we create later

---
+++ Real-Time_Siddur/Errors.swift
@@ -0,0 +1,13 @@
+//
+//  Errors.swift
+//  Real-Time_Siddur
+//
+//  Created by Ezra Muskat on 8/21/19.
+//  Copyright © 2019 Ezra Muskat. All rights reserved.
+//
+
+import Foundation
+
+enum Errors: Error {
+    case stdError
+}
