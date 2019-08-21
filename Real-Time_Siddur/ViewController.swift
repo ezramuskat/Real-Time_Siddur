6a094bb76c04727ff3f565467d21359a712b5e85
Ezra Muskat <razeprime1@gmail.com>
2019-08-21 13:15:57 -0400

    Started building a basic interface for testing DateAnalyzer

--- Real-Time_Siddur/ViewController.swift
+++ Real-Time_Siddur/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leapYearLabel: UIView!
+    @IBOutlet weak var monthLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
