8c14018b1e9ba1b3b7676d98f65615ed69de3aa8
Ezra Muskat <razeprime1@gmail.com>
2019-12-15 13:39:51 +0200

    Restructured files for easier access and added a HolidayData struct to easily pass around holiday data

---
+++ Real-Time_Siddur/Date Analysis/HolidayData.swift
@@ -0,0 +1,16 @@
+//
+//  HolidayData.swift
+//  Real-Time_Siddur
+//
+//  Created by Ezra Muskat on 12/15/19.
+//  Copyright © 2019 Ezra Muskat. All rights reserved.
+//
+
+import Foundation
+
+struct HolidayData {
+    let isHoliday: Bool
+    let holiday: Holiday
+    let holidayName: String
+    let dayOf: Int?
+}
